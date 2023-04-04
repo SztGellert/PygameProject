8cb79ce95d617654ab6c6ff76bd83c8bf0f279e6
SztGellert <sztankgellert@gmail.com>
2023-04-04 16:10:41 +0200

    exit function

--- main.py
+++ main.py
@@ -7,3 +7,9 @@ import pygame
 if __name__ == '__main__':
    pygame.init()
    screen = pygame.display.set_mode((800, 400))
+
+   while True:
+      for event in pygame.event.get():
+         if event.type == pygame.QUIT:
+            pygame.quit()
+      pygame.display.update()
