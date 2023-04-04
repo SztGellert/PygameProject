c78b6bd8bfefb11bcc597af5f3714e4949e3eef3
SztGellert <sztankgellert@gmail.com>
2023-04-04 22:19:16 +0200

    added rectangles

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ if __name__ == '__main__':
     clock = pygame.time.Clock()
     test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
     sky_surface = pygame.image.load('graphics/Sky.png')
-    ground_surface = pygame.image.load('graphics/ground.png')
-    text_surface = test_font.render('My game', False, 'Black')
-    snail_surface = pygame.image.load('graphics/snail/snail1.png')
-    snail_x_pos = 600
+    ground_surface = pygame.image.load('graphics/ground.png').convert_alpha()
+    text_surface = test_font.render('My game', False, 'Black').convert_alpha()
+    player_surf = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
+    player_rect = player_surf.get_rect(midbottom = (80, 300))
+    snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+    snail_rect = snail_surface.get_rect(midbottom = (160, 300))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -20,9 +22,10 @@ if __name__ == '__main__':
             screen.blit(sky_surface, (0,0))
             screen.blit(ground_surface, (0,300))
             screen.blit(text_surface, (300,50))
-            screen.blit(snail_surface, (snail_x_pos, 250))
-            snail_x_pos -= 4
-            if snail_x_pos < -100:
-                snail_x_pos = 800
+            if snail_rect.left < -100: snail_rect.left = 800
+            screen.blit(player_surf, player_rect)
+            screen.blit(snail_surface, snail_rect)
+            snail_rect.left -= 3
+
         pygame.display.update()
         clock.tick(60)
