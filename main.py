5a2f3a48016f86257c905f7e5f4c0355691dda24
SztGellert <sztankgellert@gmail.com>
2023-04-04 20:40:16 +0200

    use text on surface

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ if __name__ == '__main__':
     screen = pygame.display.set_mode((800, 400))
     pygame.display.set_caption('Runner')
     clock = pygame.time.Clock()
-
+    test_font = pygame.font.Font(None, 50)
     sky_surface = pygame.image.load('graphics/Sky.png')
     ground_surface = pygame.image.load('graphics/ground.png')
-
+    text_surface = test_font.render('My game', False, 'Green')
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -17,6 +17,6 @@ if __name__ == '__main__':
                exit()
             screen.blit(sky_surface, (0,0))
             screen.blit(ground_surface, (0,300))
-
+            screen.blit(text_surface, (300,50))
         pygame.display.update()
         clock.tick(60)
